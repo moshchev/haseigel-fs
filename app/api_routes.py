2f75cf3ac5d23e3054c7bab667c0c930248604c8
Alexander Moshchev <118296062+moshchev@users.noreply.github.com>
2024-11-04 16:22:56 +0100

    new endpoint to classify images

--- app/api_routes.py
+++ app/api_routes.py
@@ -1,7 +1,7 @@
 from flask import request, jsonify
 from app import app  # Import the Flask app instance
 from app.services.processing_functions import process_domains
-
+from app.services.single_image_classification import classify_image
 @app.route('/process-domains', methods=['POST'])
 def process_domains_endpoint():
     try:
@@ -17,6 +17,27 @@ def process_domains_endpoint():
     except Exception as e:
         return jsonify({'error': str(e)}), 400
     
+
 @app.route('/health', methods=['GET'])
 def health_check():
     return jsonify({'status': 'ok'}), 200
+
+
+@app.route('/model/<model_name>', methods=['POST'])
+def model_classification(model_name):
+    try:
+        if 'image' not in request.files:
+            return jsonify({'error': 'No image file provided'}), 400
+            
+        image_file = request.files['image']
+        if image_file.filename == '':
+            return jsonify({'error': 'No selected file'}), 400
+            
+        # Call your classification service
+        result = classify_image(image_file, model_name)
+        return jsonify(result), 200
+        
+    except ValueError as ve:
+        return jsonify({'error': str(ve)}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
