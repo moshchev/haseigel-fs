48e43abfa376e2080df2729527c42318fa58b22c
Alexander Moshchev <118296062+moshchev@users.noreply.github.com>
2024-11-24 20:30:07 +0100

    test folders

---
+++ tests/test_openai.py
@@ -0,0 +1,14 @@
+import os
+import sys
+# Get the absolute path to the project root
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, project_root)
+
+from app.services.image_models import OpenAIImageClassifier
+
+image_path = 'data/images/temp/0_1156EF.jpeg-200x200.jpg'
+
+model = OpenAIImageClassifier()
+# categories = ["cat", "dog", "bird"]
+prediction = model.predict(image_path)
+print(prediction)
