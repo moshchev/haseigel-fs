288ec8540ee7b780d86a554329477cbfded42634
Alexander Moshchev <118296062+moshchev@users.noreply.github.com>
2024-11-24 18:56:16 +0100

    added new class to init

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,4 +1,7 @@
 from .image_preprocessing import prepare_image
-from .prompts import create_dynamic_schema, ImagePrompt
+from .prompts import create_dynamic_schema, ImagePrompt, NoCategoriesSchema
 
-__all__ = ["prepare_image", "create_dynamic_schema", "ImagePrompt"]
+__all__ = ["prepare_image", 
+           "create_dynamic_schema", 
+           "ImagePrompt", 
+           "NoCategoriesSchema"]
