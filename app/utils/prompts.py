b802f696ef97977c9a5a06e34c25766cf3c1d880
Alexander Moshchev <118296062+moshchev@users.noreply.github.com>
2024-11-24 12:10:41 +0100

    second prompt

--- app/utils/prompts.py
+++ app/utils/prompts.py
@@ -20,3 +20,14 @@ class ImagePrompt():
     - For each category, respond with `true` if the object is clearly visible, and `false` otherwise.
     - If none of the categories match the content of the image, assign your own single-word category to describe the image.
     """
+    NO_SCHEMA_PROMPT = """
+    You are an advanced AI system specializing in image recognition.
+    Your task is to analyze the provided image and determine the most appropriate category for the image.
+    **Instructions:**
+    - Analyze the image and provide a single category that best describes the content of the image.
+    - Return the result in a JSON format with the key 'prediction' and the value as the category.
+    
+    EXAMPLE_JSON = {
+        "prediction": predicted category,
+    }
+    """
