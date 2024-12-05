1c68b93373b432d967a16a63515b0075d7358037
Alexander Moshchev <118296062+moshchev@users.noreply.github.com>
2024-12-05 11:25:53 +0100

    litetllm completeion

--- tests/fireworks_ai.py
+++ tests/fireworks_ai.py
@@ -3,37 +3,37 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# response = completion(
-#     model="fireworks_ai/accounts/fireworks/models/llama-v3p1-8b-instruct", 
-#     messages=[
-#        {"role": "user", "content": "hello from litellm"}
-#    ],
-# )
+response = completion(
+    model="fireworks_ai/accounts/fireworks/models/llama-v3p1-8b-instruct", 
+    messages=[
+       {"role": "user", "content": "hello from litellm"}
+   ],
+)
 
-# print(response)
+print(response)
 
-import requests
-import json
-import os
+# import requests
+# import json
+# import os
 
-url = "https://api.fireworks.ai/inference/v1/chat/completions"
-payload = {
-  "model": "accounts/fireworks/models/llama-v3p1-70b-instruct",
-  "max_tokens": 4096,
-  "top_p": 1,
-  "top_k": 40,
-  "presence_penalty": 0,
-  "frequency_penalty": 0,
-  "temperature": 0.6,
-  "messages": [
-    {"role": "user", "content": "hello from frankfurt"}
-  ]
-}
-headers = {
-  "Accept": "application/json",
-  "Content-Type": "application/json",
-  "Authorization": f"Bearer {os.getenv('FIREWORKS_AI_API_KEY')}"
-}
-response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
+# url = "https://api.fireworks.ai/inference/v1/chat/completions"
+# payload = {
+#   "model": "accounts/fireworks/models/llama-v3p1-70b-instruct",
+#   "max_tokens": 4096,
+#   "top_p": 1,
+#   "top_k": 40,
+#   "presence_penalty": 0,
+#   "frequency_penalty": 0,
+#   "temperature": 0.6,
+#   "messages": [
+#     {"role": "user", "content": "hello from frankfurt"}
+#   ]
+# }
+# headers = {
+#   "Accept": "application/json",
+#   "Content-Type": "application/json",
+#   "Authorization": f"Bearer {os.getenv('FIREWORKS_AI_API_KEY')}"
+# }
+# response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
 
-print(response.text)
+# print(response.text)
